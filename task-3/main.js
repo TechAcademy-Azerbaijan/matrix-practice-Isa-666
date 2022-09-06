3d9e17b7666aab15323cef3e7e0f7739f94cde27
Isa <110451592+Isa-666@users.noreply.github.com>
2022-09-06 11:25:54 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,4 +1,3 @@
-// Use this matrix for solution
 
 let matrix = [
     [2, 5, 3, 1, 4],
@@ -6,5 +5,17 @@ let matrix = [
     [4, 6, 1, 2, 3],
     [4, 5, 6, 7, 7]
 ]
+let sum=0
+let max_index=0
 
-// Write code here. 
+for (let i = 0; i < matrix.length; i++) {
+   for (let j = 0; j < matrix[i].length; j++) {
+
+    if (matrix[i][j]==1) {
+        max_index=[i+1]
+        console.log(max_index.join(' '));
+    }
+    
+   }
+    
+}
